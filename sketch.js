2fa5cc1d827abce33c6ac7a08bf0beebf5f51dae
julietM <30603889+julietM@users.noreply.github.com>
2018-03-09 14:36:02 -0500

    crear archivo

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+function setup(){
+   createCanvas(3560,650);
+   background(2,120,27)
+}
+
+
+function draw() {
+   fill(246,246,65); //cara
+   ellipse(400,300,320,320);
+
+    fill(13);//eyes
+   ellipse(350,250,60,60);
+
+   fill(13);//eyes
+   ellipse(450,250,60,60);
+
+   fill(109,42,11);//boca
+   ellipse(410,340,80,30)
+ 
+
+
+   
+
+
+}
